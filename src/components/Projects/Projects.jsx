4420ab58c52324368b4307d1e6807c914ba0e9bd
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-09-25 00:43:07 +0600

    added project section theme

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,11 +4,12 @@ import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import "./Projects.css";
 import TabItem from '../TabItem/TabItem';
-import classNames from 'classnames';
+import useAuth from '../hooks/useAuth';
 
 const Projects = () => {
 
     const [projects, setProjects] = useState([]);
+    const { themeBlack } = useAuth();
 
     useEffect(() => {
         fetch("/projects.json")
@@ -23,69 +24,62 @@ const Projects = () => {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
-        <div className='max-w-[1120px] mx-auto my-32 border'>
-            <SectionTitle title={"Portfolio"} subTitle={"My Work & Projects"}></SectionTitle>
-            <div className='mt-20 px-4'>
-                <Tabs selectedIndex={activeTab} onSelect={index => setActiveTab(index)}>
-                    <TabList className="tab-list">
-                        <div className='text-center'>
-                            <Tab
-                                className={classNames("tab", { 'border-b-4 bg-white': activeTab === 0 })}>  ALL
-                            </Tab>
-                            <Tab
-                                className={classNames("tab", { 'border-b-4 bg-white': activeTab === 1 })}>MERN
-                            </Tab>
-                            <Tab
-                                className={classNames("tab", { 'border-b-4 bg-white': activeTab === 2 })}>REACT
-                            </Tab>
-                            <Tab
-                                className={classNames("tab", { 'border-b-4 bg-white': activeTab === 3 })}
-                            >HTML/CSS
-                            </Tab>
-                        </div>
-                    </TabList>
+        <div className={themeBlack == true ? "bg-[#1E293B] text-[#94A3B8] duration-700" : "bg-white duration-700 text-black"}>
+            <div className='max-w-[1120px] mx-auto py-32 '>
+                <SectionTitle title={"Portfolio"} subTitle={"My Work & Projects"}></SectionTitle>
+                <div className='mt-20 px-4'>
+                    <Tabs >
+                        <TabList>
+                            <div className='text-center text-xl font-semibold '>
+                                <Tab>ALL</Tab>
+                                <Tab>MERN</Tab>
+                                <Tab>REACT</Tab>
+                                <Tab>HTML/CSS</Tab>
+                            </div>
+                        </TabList>
 
-                    <TabPanel>
-                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20'>
-                            {
-                                projects.map(project => <TabItem
-                                    key={project._id}
-                                    project={project}
-                                ></TabItem>)
-                            }
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20'>
-                            {
-                                mern.map(project => <TabItem
-                                    key={project._id}
-                                    project={project}
-                                ></TabItem>)
-                            }
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20'>
-                            {
-                                react.map(project => <TabItem
-                                    key={project._id}
-                                    project={project}
-                                ></TabItem>)
-                            }
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20'>
-                            {
-                                html.map(project => <TabItem
-                                    key={project._id}
-                                    project={project}
-                                ></TabItem>)
-                            }
-                        </div>
-                    </TabPanel>
-                </Tabs>
+                        <TabPanel>
+                            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20'>
+                                {
+                                    projects.map(project => <TabItem
+                                        key={project._id}
+                                        project={project}
+                                    ></TabItem>)
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20'>
+                                {
+                                    mern.map(project => <TabItem
+                                        key={project._id}
+                                        project={project}
+                                    ></TabItem>)
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20'>
+                                {
+                                    react.map(project => <TabItem
+                                        key={project._id}
+                                        project={project}
+                                    ></TabItem>)
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20'>
+                                {
+                                    html.map(project => <TabItem
+                                        key={project._id}
+                                        project={project}
+                                    ></TabItem>)
+                                }
+                            </div>
+                        </TabPanel>
+                    </Tabs>
+                </div>
             </div>
         </div>
     );
